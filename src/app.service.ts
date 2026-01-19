139606541d8f3e36398c315aad1a4b6b2ef06f61
Chi Bao Bao <chibaobao@192.168.2.59>
2026-01-19 20:33:27 +0700

    Check && Console.log the MongoDB connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  // Inject the Mongoose connection
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  onModuleInit() {
+    // Sử dụng ép kiểu hoặc kiểm tra trạng thái từ Connection
+    const isConnected = this.connection.readyState;
+    // readyState: 1 = connected, 0 = disconnected, 2 = connecting, 3 = disconnecting, 99 = uninitialized
+
+    if (isConnected) {
+      console.log('✅ MongoDB đã kết nối thành công!');
+    } else {
+      console.log(
+        '❌ MongoDB chưa kết nối. Trạng thái:',
+        this.connection.readyState,
+      );
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
